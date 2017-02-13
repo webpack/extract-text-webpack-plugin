7940c447e6246d428c46070cacfcb0302817e576
Sresan Thevarajah <sresan.thevarajah@redfin.com>
2017-02-13 11:07:19 -0800

    content not context...

--- index.js
+++ index.js
@@ -269,7 +269,7 @@ ExtractTextPlugin.prototype.apply = function(compiler) {
 									return callback();
 								}
 								meta = module[NS];
-								if(!Array.isArray(meta.content) && meta.context != null) {
+								if(!Array.isArray(meta.content) && meta.content != null) {
 									err = new Error(module.identifier() + " doesn't export content");
 									compilation.errors.push(err);
 									return callback();
