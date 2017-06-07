43957d36c725214bd93b6469dc62ec2c813e0390
Michael Ciniawsky <michael.ciniawsky@gmail.com>
2017-06-08 01:19:09 +0200

    refactor: prepare for `webpack-defaults`

--- test/cases/merging-multiple-entries-shared/webpack.config.js
+++ test/cases/merging-multiple-entries-shared/webpack.config.js
@@ -1,13 +1,14 @@
 var ExtractTextPlugin = require("../../../");
+
 module.exports = {
-	entry: {
-		a: "./a",
-		b: "./b"
-	},
-	plugins: [
-		new ExtractTextPlugin({
-			filename: "[name].txt",
-			allChunks: true
-		})
-	]
+  entry: {
+    a: "./a",
+    b: "./b"
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "[name].txt",
+      allChunks: true
+    })
+  ]
 };
--- test/cases/splitted-chunk/webpack.config.js
+++ test/cases/splitted-chunk/webpack.config.js
@@ -1,10 +1,11 @@
 var ExtractTextPlugin = require("../../../");
+
 module.exports = {
-	entry: "./index",
-	plugins: [
-		new ExtractTextPlugin({
-			filename: "file.css",
-			allChunks: false
-		})
-	]
+  entry: "./index",
+  plugins: [
+    new ExtractTextPlugin({
+      filename: "file.css",
+      allChunks: false
+    })
+  ]
 };
