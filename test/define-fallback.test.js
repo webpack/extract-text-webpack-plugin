233757df85c66eba8d4e4cddf78076a21fbde974
Joshua Wiens <joshuaw@easymetrics.com>
2017-06-11 13:16:32 -0500

    style: Single quotes in specs

--- test/define-fallback.test.js
+++ test/define-fallback.test.js
@@ -1,65 +1,65 @@
-import ExtractTextPlugin from "../src";
-const loader = require.resolve("../src/loader.js");
+import ExtractTextPlugin from '../src';
+const loader = require.resolve('../src/loader');
 
 
 describe('Define Fallback', () => {
-  it("accepts a fallback string", () => {
+  it('accepts a fallback string', () => {
     expect(ExtractTextPlugin.extract({
-      fallback: "style-loader",
-      use: "css-loader"
+      fallback: 'style-loader',
+      use: 'css-loader'
     })).toEqual([
       { loader, options: { omit: 1, remove: true } },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 
-  it("accepts a fallback string with legacy fallbackLoader option", () => {
+  it('accepts a fallback string with legacy fallbackLoader option', () => {
     expect(ExtractTextPlugin.extract({
-      fallback: "style-loader",
-      use: "css-loader"
+      fallback: 'style-loader',
+      use: 'css-loader'
     })).toEqual([
       { loader, options: { omit: 1, remove: true } },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 
-  it("accepts a chained fallback string", () => {
+  it('accepts a chained fallback string', () => {
     expect(ExtractTextPlugin.extract({
-      fallback: "something-loader!style-loader",
-      use: "css-loader"
+      fallback: 'something-loader!style-loader',
+      use: 'css-loader'
     })).toEqual([
       { loader, options: { omit: 2, remove: true } },
-      { loader: "something-loader" },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader: 'something-loader' },
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 
-  it("accepts a fallback object", () => {
+  it('accepts a fallback object', () => {
     expect(ExtractTextPlugin.extract({
-      fallback: { loader: "style-loader" },
-      use: "css-loader"
+      fallback: { loader: 'style-loader' },
+      use: 'css-loader'
     })).toEqual([
       { loader, options: { omit: 1, remove: true } },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 
-  it("accepts an array of fallback objects", () => {
+  it('accepts an array of fallback objects', () => {
     expect(ExtractTextPlugin.extract({
       fallback: [
-        { loader: "something-loader" },
-        { loader: "style-loader" }
+        { loader: 'something-loader' },
+        { loader: 'style-loader' }
       ],
-      use: "css-loader"
+      use: 'css-loader'
     })).toEqual([
       { loader, options: { omit: 2, remove: true } },
-      { loader: "something-loader" },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader: 'something-loader' },
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 });
--- test/extract.test.js
+++ test/extract.test.js
@@ -1,23 +1,23 @@
-import ExtractTextPlugin from "../src";
-const loader = require.resolve("../src/loader.js");
+import ExtractTextPlugin from '../src';
+const loader = require.resolve('../src/loader');
 
 
-describe("ExtractTextPlugin.extract()", () => {
-  it("throws if given multiple arguments", () => {
+describe('ExtractTextPlugin.extract()', () => {
+  it('throws if given multiple arguments', () => {
     expect(() => {
-      ExtractTextPlugin.extract("style-loader", "css-loader");
+      ExtractTextPlugin.extract('style-loader', 'css-loader');
     }).throws;
   });
 
-  it("passes additional options to its own loader", () => {
+  it('passes additional options to its own loader', () => {
     expect(ExtractTextPlugin.extract({
-      fallback: "style-loader",
-      use: "css-loader",
-      publicPath: "/test"
+      fallback: 'style-loader',
+      use: 'css-loader',
+      publicPath: '/test'
     })).toEqual([
-      { loader, options: { omit: 1, remove: true, publicPath: "/test" } },
-      { loader: "style-loader" },
-      { loader: "css-loader" }
+      { loader, options: { omit: 1, remove: true, publicPath: '/test' } },
+      { loader: 'style-loader' },
+      { loader: 'css-loader' }
     ]);
   });
 });
