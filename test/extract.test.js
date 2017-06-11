249969f09a06cda3580b6e7ba0f9484b64781ec2
Joshua Wiens <joshuaw@easymetrics.com>
2017-06-11 01:59:21 -0500

    test: Convert plugin tests to Jest

---
+++ test/extract.test.js
@@ -0,0 +1,24 @@
+import ExtractTextPlugin from "../src";
+const loader = require.resolve("../src/loader.js");
+
+
+describe("ExtractTextPlugin.extract()", () => {
+  it("throws if given multiple arguments", () => {
+    expect(() => {
+      ExtractTextPlugin.extract("style-loader", "css-loader");
+    }).throws;
+  });
+
+  it("passes additional options to its own loader", () => {
+    expect(ExtractTextPlugin.extract({
+      fallback: "style-loader",
+      use: "css-loader",
+      publicPath: "/test"
+    })).toEqual([
+      { loader, options: { omit: 1, remove: true, publicPath: "/test" } },
+      { loader: "style-loader" },
+      { loader: "css-loader" }
+    ]);
+  });
+});
+
