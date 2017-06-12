466ffbf36043b0e5e00fbb8e60dc7dff397c1643
Joshua Wiens <joshuaw@easymetrics.com>
2017-06-12 05:24:35 -0500

    refactor: Use indexOf in helpers

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -3,8 +3,8 @@ export function isInitialOrHasNoParents(chunk) {
 }
 
 export function isInvalidOrder(a, b) {
-  const bBeforeA = a.getPrevModules().includes(b);
-  const aBeforeB = b.getPrevModules().includes(a);
+  const bBeforeA = a.getPrevModules().indexOf(b) >= 0;
+  const aBeforeB = b.getPrevModules().indexOf(a) >= 0;
   return aBeforeB && bBeforeA;
 }
 
@@ -17,8 +17,8 @@ export function getOrder(a, b) {
   const bIndex = b.getOriginalModule().index2;
   if (aIndex < bIndex) return -1;
   if (aIndex > bIndex) return 1;
-  const bBeforeA = a.getPrevModules().includes(b);
-  const aBeforeB = b.getPrevModules().includes(a);
+  const bBeforeA = a.getPrevModules().indexOf(b) >= 0;
+  const aBeforeB = b.getPrevModules().indexOf(a) >= 0;
   if (aBeforeB && !bBeforeA) return -1;
   if (!aBeforeB && bBeforeA) return 1;
   const ai = a.identifier();
