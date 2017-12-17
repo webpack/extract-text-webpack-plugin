4bc428c1499ce0347c13451b22d63bdfac116089
tennylv <441403517@qq.com>
2017-12-17 18:29:55 +0800

    fix https://github.com/webpack-contrib/extract-text-webpack-plugin/issues/68 6

--- src/index.js
+++ src/index.js
@@ -143,7 +143,7 @@ class ExtractTextPlugin {
         });
         async.forEach(chunks, (chunk, callback) => { // eslint-disable-line no-shadow
           const extractedChunk = extractedChunks[chunks.indexOf(chunk)];
-          const shouldExtract = !!(options.allChunks || isInitialOrHasNoParents(chunk));
+          const shouldExtract = !!(options.allChunks);
           chunk.sortModules();
           async.forEach(chunk.mapModules(c => c), (module, callback) => { // eslint-disable-line no-shadow
             let meta = module[NS];
@@ -152,7 +152,16 @@ class ExtractTextPlugin {
               // A stricter `shouldExtract !== wasExtracted` check to guard against cases where a previously extracted
               // module would be extracted twice. Happens when a module is a dependency of an initial and a non-initial
               // chunk. See issue #604
-              if (shouldExtract && !wasExtracted) {
+
+              let cks = module.chunks;
+              // check every module's chunks.parents() to decide extract or not
+              for (let i = 0 ; i < cks.length ; i++) {
+                  if (!isInitialOrHasNoParents(cks[i]) && !module.noExtracted) {
+                      module.noExtracted = 1;
+                      break;
+                  }
+              }
+              if (shouldExtract || (!module.noExtracted && !wasExtracted)) {
                 module[`${NS}/extract`] = shouldExtract; // eslint-disable-line no-path-concat
                 compilation.rebuildModule(module, (err) => {
                   if (err) {
